3fcfcaed90aa1e3dbf64b4cd198450683349f50f
gustavobacellarladeira <gustavo.bacellar20@gmail.com>
2020-09-15 00:16:16 -0300

    logos

--- CRUD/frontend/src/components/template/Logo.jsx
+++ CRUD/frontend/src/components/template/Logo.jsx
@@ -1,7 +1,9 @@
 import './Logo.css'
-import React from 'react'
+import logo from '../../assets/img/logo.png'
 
 export default props =>
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src="{logo}" alt="logo"/>
+        </a>
     </aside>
